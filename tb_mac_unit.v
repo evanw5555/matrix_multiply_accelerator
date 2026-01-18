26b2a0253bcfd3e5c93ce1a717cd713ec38126ec
Evan W <78935880+evanw5555@users.noreply.github.com>
2026-01-17 20:55:13 -0500

    Clean up comments in tb_mac_unit.v
    Removed comments related to clock generation and signal initialization.

--- tb_mac_unit.v
+++ tb_mac_unit.v
@@ -17,21 +17,17 @@ module tb_mac_unit();
         .acc(acc)
     );
 
-    // Clock generator
     always #5 clk = ~clk;
 
     initial begin
-        // Initialize signals
         clk = 0;
         reset = 1;
         enable = 0;
         a = 0;
         b = 0;
 
-        // Hold reset for 10 ns
         #10 reset = 0;
 
-        // Enable the MAC
         enable = 1;
 
         // Apply inputs
